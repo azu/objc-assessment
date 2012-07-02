1b04896178607463a10c548ebdf0ce80cc0fec20
azu <azuciao@gmail.com>
2012-07-03 01:24:01 +0900

    init

---
+++ objc-assessmentTests/NSStringTests.h
@@ -0,0 +1,13 @@
+//
+//  NSStringTests.h
+//  objc-assessment
+//
+//  Created by azu on 07/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSStringTests : SenTestCase
+
+@end
---
+++ objc-assessmentTests/NSStringTests.m
@@ -0,0 +1,14 @@
+//
+//  NSStringTests.m
+//  objc-assessment
+//
+//  Created by azu on 07/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSStringTests.h"
+
+@implementation NSStringTests
+
+
+@end
